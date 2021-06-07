cba93974efd0cdc9d2026c38e60937430cac3231
Polaris-dev9 <devpolaris9@gmail.com>
2021-06-07 21:17:43 +0545

    Simple Change

--- src/components/App.js
+++ src/components/App.js
@@ -6,18 +6,6 @@ import Header from "./Header";
 import AddButton from "./AddButton";
 import ListContainer from "./ListContainer";
 import Footer from "./Footer";
-//All about the data list
-// const taskObjs = [
-//   {
-//     title: "Learning React JS.",
-//     desc: "Today I will be learning the basics of the React JS.",
-//   },
-//   {
-//     title: "Robotics Task.",
-//     desc: "Task 1 to be completed by 4pm.",
-//   },
-// ];
-
 const App = () => {
   const [taskObjs, setTaskObjs] = useState([]);
   const updateTaskObj = (datas) => {
