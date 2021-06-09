34f1a18452477640171c620a2cd47ca5a0c22676
Polaris-dev9 <devpolaris9@gmail.com>
2021-06-09 17:55:12 +0545

    Edit functionality completed

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -2,16 +2,32 @@ import React, { useState } from "react";
 
 const Edit = (props) => {
   const { editableObj } = props;
-  console.log(editableObj);
+  const { title, desc } = editableObj;
+  const [title1, setTitle1] = useState(title);
+  const [desc1, setDesc1] = useState(desc);
   return (
     <>
       <div className="container-750">
         <form>
           <div className="form__container">
-            <input type="text" placeholder="Title" />
-            <input type="text" placeholder="Description" />
+            <input
+              type="text"
+              placeholder="Title"
+              value={title1}
+              onChange={(e) => {
+                setTitle1(e.target.value);
+              }}
+            />
+            <input
+              type="text"
+              placeholder="Description"
+              value={desc1}
+              onChange={(e) => {
+                setDesc1(e.target.value);
+              }}
+            />
           </div>
-          <button className="btn-primary">+ Edit the Task.</button>
+          <button className="btn-primary">Okey Done Editing!</button>
         </form>
       </div>
     </>
